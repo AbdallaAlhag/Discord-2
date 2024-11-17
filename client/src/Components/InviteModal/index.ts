d3ecda7158e7b640834b93f312b87c5b1effd343
Abdalla Alhag <alhagabdalla@gmail.com>
2024-11-16 19:52:48 -0800

    Remove all cached files from index and reapply .gitignore rules

--- client/src/Components/InviteModal/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as InviteModal } from './InviteModal';
-export { FriendItem } from './FriendItem';
-export { SearchInput } from './SearchInput';
--- client/src/Pages/index.ts
+++
@@ -1,6 +0,0 @@
-import HomePage from "./HomePage";
-import ServerPage from "./ServerPage";
-import {LoginPage} from "./LoginPage";
-import {RegisterPage} from "./RegisterPage";
-
-export { HomePage, ServerPage, LoginPage, RegisterPage }
