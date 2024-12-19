097c12d94f388890f3ac02caeaea635a3953eabe
Abdalla Alhag <alhagabdalla@gmail.com>
2024-12-19 11:51:15 -0800

    remove nitro and shop from friend sidebar

--- client/src/Components/Home/FriendSidebar.tsx
+++ client/src/Components/Home/FriendSidebar.tsx
@@ -1,4 +1,4 @@
-import { Users, Plus, Store, Gauge } from "lucide-react";
+import { Users, Plus } from "lucide-react";
 import SettingsButton from "../Profile/SettingsButton";
 import LogoutButton from "../Profile/LogoutButton";
 import { useAuth } from "@/AuthContext";
@@ -122,20 +122,6 @@ export default function FriendSidebar({
               <span className="text-lg font-semibold">Friends</span>
             </button>
           </Link>
-          <button
-            className="w-full flex items-center px-2 py-2 text-[#dcddde] hover:bg-[#42464D] rounded group"
-            // onClick={() => toggleChatSection(null)}
-          >
-            <Gauge className="w-5 h-5 mr-4" />
-            <span className="text-lg font-semibold">Nitro</span>
-          </button>
-          <button
-            className="w-full flex items-center px-2 py-2 text-[#dcddde] hover:bg-[#42464D] rounded group"
-            // onClick={() => toggleChatSection(null)}
-          >
-            <Store className="w-5 h-5 mr-4" />
-            <span className="text-lg font-semibold">Shop</span>
-          </button>
         </div>
 
         <div className="px-2 pt-4">
