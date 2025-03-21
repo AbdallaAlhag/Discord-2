6b3959e8986f70265d8bb6f0fa7e1af2bc8818ff
Abdalla Alhag <alhagabdalla@gmail.com>
2025-03-21 12:28:02 -0700

    change discord-2 to discochatapp.netlify.app in backend index file

--- server/src/index.ts
+++ server/src/index.ts
@@ -24,7 +24,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://discord-2.netlify.app",
+      "https://discochatapp.netlify.app",
       "https://discord-2-production.up.railway.app",
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],
@@ -53,7 +53,7 @@ const io = new Server(server, {
   cors: {
     origin: [
       "http://localhost:5173",
-      "https://discord-2.netlify.app",
+      "https://discochatapp.netlify.app",
       "https://discord-2-production.up.railway.app",
     ],
     methods: ["GET", "POST"],
