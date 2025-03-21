b042a4e45b7ada227ba26dc4be6af7e80db66c19
Abdalla Alhag <alhagabdalla@gmail.com>
2025-03-21 12:24:58 -0700

    comment out google adsense and rename site to remove phishing message

--- client/src/Components/AdComponent.tsx
+++ client/src/Components/AdComponent.tsx
@@ -1,37 +1,37 @@
-import { useEffect } from "react";
+// import { useEffect } from "react";
 
-interface AdComponentProps {
-  adClient: string;
-  adSlot: string;
-  adFormat?: string;
-  fullWidthResponsive?: boolean;
-}
+// interface AdComponentProps {
+//   adClient: string;
+//   adSlot: string;
+//   adFormat?: string;
+//   fullWidthResponsive?: boolean;
+// }
 
 
-const AdComponent: React.FC<AdComponentProps> = ({
-  adClient,
-  adSlot,
-  adFormat = "auto",
-  fullWidthResponsive = true,
-}) => {
-  useEffect(() => {
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    const adsbygoogle = (window as any).adsbygoogle ?? [];
-    adsbygoogle.push({});
-  }, []);
+// const AdComponent: React.FC<AdComponentProps> = ({
+//   adClient,
+//   adSlot,
+//   adFormat = "auto",
+//   fullWidthResponsive = true,
+// }) => {
+//   useEffect(() => {
+//     // eslint-disable-next-line @typescript-eslint/no-explicit-any
+//     const adsbygoogle = (window as any).adsbygoogle ?? [];
+//     adsbygoogle.push({});
+//   }, []);
 
-  return (
-    <div>
-      <ins
-        className="adsbygoogle"
-        style={{ display: "block" }}
-        data-ad-client={adClient}
-        data-ad-slot={adSlot}
-        data-ad-format={adFormat}
-        data-full-width-responsive={fullWidthResponsive ? "true" : "false"}
-      ></ins>
-    </div>
-  );
-};
+//   return (
+//     <div>
+//       <ins
+//         className="adsbygoogle"
+//         style={{ display: "block" }}
+//         data-ad-client={adClient}
+//         data-ad-slot={adSlot}
+//         data-ad-format={adFormat}
+//         data-full-width-responsive={fullWidthResponsive ? "true" : "false"}
+//       ></ins>
+//     </div>
+//   );
+// };
 
-export default AdComponent;
+// export default AdComponent;
